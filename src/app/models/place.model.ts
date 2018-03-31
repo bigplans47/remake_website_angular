a852ad3ca0bd2f5bd446cbfc8c1060a8eae48b2d
Dan Kiss <flowfast47@gmail.com>
2018-03-30 19:00:31 -0700

    add model for tourist destinations

---
+++ src/app/models/place.model.ts
@@ -0,0 +1,5 @@
+export class Place {
+  public location: string = 'Budapest';
+  public reviews: number = 100;
+  constructor(public name: string, public rating: number, public picture: string) {}
+}
